8eb05ad10be5cdf514fa5bdcc9f0017ae811e377
Selin Dündar <selin.dundar9@gmail.com>
2021-02-14 20:56:55 +0200

    20 Ocak 2020 (4)

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+//sepete ekleme işlemi 3. ders
+{   //naming convention 
+    class SepetManager
+    {
+    
+        public void Ekle(Urun urun)
+			{
+            Console.WriteLine("tebrikler.sepete eklendi : " + urun.Adi);
+			}
+
+        public void Ekle2(string urunAdi, string aciklama , double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("tebrikler.sepete eklendi : " + urunAdi);
+        }
+
+
+
+
+    }
+
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+               
+
---
+++ StudyForHomework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// 
+namespace StudyForHomework
+{   //0 girilene kadar sayıların toplamını  bulan program 18-01-21
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi = 1;
+            //int toplam = 0;
+            
+         
+           // while (sayi != 0)
+            //{
+               
+                //Console.Write("sayı gir emmi");
+                //sayi = Convert.ToInt32(Console.ReadLine());
+               
+
+                //toplam  =toplam + sayi;
+
+            //}
+            //Console.WriteLine("toplam:"+toplam + "ediyor ");
+            //Console.ReadKey();
+
+
+            // ekrana 0 girilene kadar yazdıran kod
+            int sayi = 1;
+            while (sayi!= 0) ;
+            {
+                Console.Write("please,enter a number---> ");
+                Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("entered number= {0}", sayi);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
